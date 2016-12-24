e2a729ab8949278e60e9c6ad1df668ae145eb73e
DESKTOP-FT76O01\adobe <hanlam809@gmail.com>
2016-12-24 23:32:01 +0700

    fixbug for repository and add post services

--- TeduShop.Data/Repositories/FooterRepository.cs
+++ TeduShop.Data/Repositories/FooterRepository.cs
@@ -1,16 +1,45 @@
-﻿using TeduShop.Data.Infrastructure;
-using TeduShop.Model.Models;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
 
-namespace TeduShop.Data.Repositories
+namespace TeduShop.Model.Models
 {
-    public interface IFooterRepository : IRepository<Footer>
+    [Table("Posts")]
+    public class Post : Auditable
     {
-    }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
 
-    public class FooterRepository : RepositoryBase<Footer>, IFooterRepository
-    {
-        public FooterRepository(IDbFactory dbFactory) : base(dbFactory)
-        {
-        }
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        [Column(TypeName = "varchar")]
+        public string Alias { set; get; }
+
+        [Required]
+        public int CategoryID { set; get; }
+
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        [MaxLength(500)]
+        public string Description { set; get; }
+
+        public string Content { set; get; }
+
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
+
+        [ForeignKey("CategoryID")]
+        public virtual PostCategory PostCategory { set; get; }
+
+        public virtual IEnumerable<PostTag> PostTags { set; get; }
     }
 }
